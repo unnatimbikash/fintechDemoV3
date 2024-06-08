c66fade5d1839a2477ff2b61b1a5962677d5fe36
Abninandan <164477582+Abninandan@users.noreply.github.com>
2024-06-08 11:28:15 +0530

    todays work 8-06-24 at 11:28AM

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -39,6 +39,7 @@
     <script src="../js/setting.js"></script>
     <script src="../js/dashboard.js"></script>
     <script src="../js/app.js"></script>
+    <script src="../js/public.js"></script>
     <script src="../js/error.js"></script>
     <script src="../js/Chart.roundedBarCharts.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.all.min.js"></script>
