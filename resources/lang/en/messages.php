6a5bea05d1ba947c790b6f929347addd17d20da0
Abninandan <164477582+Abninandan@users.noreply.github.com>
2024-06-24 15:51:43 +0530

    blank

--- resources/lang/en/messages.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    'usersave'=>'Saved Successfully',
-    'permissionsave'=>'permission added Successfully',
-    'addrole'=>'Role added Successfully',
-    'permission'=>"Permission",
-    'assignpermission'=>'Success Fully Saved',
-    'nodata'=>'Data Not Selected',
-];
